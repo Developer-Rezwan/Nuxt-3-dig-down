abbfff98c2e53972fa954250224724d3d815a100
Developer-Rezwan <programmerrezwan@gmail.com>
2022-12-03 17:56:54 +0600

    routing

---
+++ pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink :to="{name : 'index'}">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="{name : 'about'}">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink :to="{name : 'index'}">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="{name : 'about'}">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <h2>
+      Welcome to homepage
+      <h3>hello</h3>
+    </h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  color: greenyellow;
+  h3 {
+    color: red;
+  }
+}
+</style>
