9a50a1dc2345cd3cccdd732197b23cf0c09c60d1
Developer-Rezwan <programmerrezwan@gmail.com>
2022-12-03 21:57:43 +0600

    setup postcss and tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <template>
-  <h1>Welcome to Home page</h1>
+  <h1 class="text-red-500">Welcome to Home page</h1>
 </template>
 
 <script setup>
 </script>
 
 <style lang="scss">
-h2 {
-  color: greenyellow;
-  span: {
-    color: red;
-  }
-}
-
-.router-link-active {
-  color: green;
-}
 </style>
